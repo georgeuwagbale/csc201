072900d81b4fe2bcecd19f69faa183497b058041
georgeuwagbale <george.uwagbale@pau.edu.ng>
2021-10-27 16:06:24 +0100

    First commit

---
+++ cmake-build-debug/CMakeFiles/csc205.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/csc205.dir/main.c.obj"
+  "csc205.exe"
+  "csc205.exe.manifest"
+  "csc205.pdb"
+  "libcsc205.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/csc205.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
